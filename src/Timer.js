67a5a3a1a1d8e252a7f4c614689a298cc77c2213
Frank Riccobono <F.Riccobono@gmail.com>
2018-12-16 10:57:56 -0500

    got the timer working

--- src/Timer.js
+++ src/Timer.js
@@ -1,56 +1,87 @@
-import React from 'react';
-
+import React from "react";
+import moment from "moment";
 export default class Timer extends React.Component {
-    state = {
-        runningTime: 0
+  initialDuration = moment.duration(10, "minutes")
+  state = {
+    started: false,
+    duration: this.initialDuration
+  };
+
+  handleClick = () => {
+    this.setState(state => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        var interval = 1000;
+        this.timer = setInterval(() => {
+          this.setState({
+            duration: moment.duration(
+              this.state.duration - interval,
+              "milliseconds"
+            ),
+            started: true
+          });
+        }, interval);
+      }
+      return { status: !state.status };
+    });
+  };
+
+  handleReset = () => {
+    clearInterval(this.timer);
+    this.setState({duration: this.initialDuration, started: false});
+  }
+
+  changeTime = (minutes) => {
+    if(!this.state.started){
+      this.setState({duration: this.state.duration.add(minutes, "minutes")});
     }
+  }
 
-    handleClick = () => {
-        this.setState(state => {
-          if (state.status) {
-            clearInterval(this.timer);
-          } else {
-            const startTime = Date.now() - this.state.runningTime;
-            this.timer = setInterval(() => {
-              this.setState({ runningTime: Date.now() - startTime });
-            });
-          }
-          return { status: !state.status };
-        });
-      };
+  render() {
+    return (
+      <div className="full centeralign even border">
+        <span className="tenth marno">
+          <a
+            className="fa fa-caret-up greentext fa-2x marno padmore padbottomless inverthover"
+            onClick={()=>this.changeTime(1)}
+          />
 
-    render(){
-        return             <div className="full centeralign even border">
-              <span className="tenth marno">
-                <a
-                  className="fa fa-caret-up greentext fa-2x marno padmore padbottomless inverthover"
-                  onClick="addMinute('3209946_timer')"
-                />
+          <a
+            className="fa fa-caret-down greentext fa-2x marno padmore padtopless inverthover"
+            onClick={()=>this.changeTime(-1)}
+          />
+        </span>
 
-                <a
-                  className="fa fa-caret-down greentext fa-2x marno padmore padtopless inverthover"
-                  onClick="subtractMinute('3209946_timer')"
-                />
-              </span>
+        <span className="half marno">
+          <span className={`stopwatch ${this.state.duration.seconds() < 0 ? "expired" : ""}`} id="3209946_timer">
+            {this.state.duration.seconds() < 0 ? "-" : ""}
+            {Math.abs(this.state.duration.minutes())}:
+            {Math.abs(this.state.duration.seconds()) < 10 ? "0" : ""}
+            {Math.abs(this.state.duration.seconds())}
+          </span>
+        </span>
 
-              <span className="half marno">
-                <span className="stopwatch" id="3209946_timer">
-                  10:00 {this.state.runningTime}
-                </span>
-              </span>
-
-              <span className="twofifths marno">
-                <button
-                  onClick={this.handleClick}
-                  id="3209946_timerStart"
-                  className="buttonwhite bluetext fa fa-play fa-lg"
-                />
-
-                <button
-                  onClick="resetTimer('3209946_timer')"
-                  className="buttonwhite redtext fa fa-undo fa-lg"
-                />
-              </span>
-            </div>;
-    }
+        <span className="twofifths marno">
+        {this.state.started ? 
+          <button
+            onClick={this.handleClick}
+            id="3209946_timerStart"
+            className="buttonwhite bluetext fa fa-pause fa-lg"
+          />
+          :
+          <button
+            onClick={this.handleClick}
+            id="3209946_timerStart"
+            className="buttonwhite bluetext fa fa-play fa-lg"
+          />
+        }
+          <button
+            onClick={this.handleReset}
+            className="buttonwhite redtext fa fa-undo fa-lg"
+          />
+        </span>
+      </div>
+    );
+  }
 }
