6606e7d61e5af3c95cf4d65f06269bce68345f60
Frank Riccobono <F.Riccobono@gmail.com>
2024-11-16 10:59:32 -0500

    Basic test cases

---
+++ src/Ballot.test.jsx
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("Landing Page", () => {
+  it("can open a speech ballot", () => {
+    render(<App />);
+    const startButtons = screen.getAllByText("ON MY WAY!");
+    fireEvent.click(startButtons[0]);
+
+    expect(
+      screen.queryByText("OBT Round 1 Ballot for Riccobono")
+    ).toBeInTheDocument();
+  });
+});
