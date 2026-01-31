aecc87f4e2a2ea7e9e3af883a3b4c4adf9131f98
Frank Riccobono <F.Riccobono@gmail.com>
2026-01-31 16:24:40 -0500

    Fix react router warning

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Layout from "../Layout.jsx";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
