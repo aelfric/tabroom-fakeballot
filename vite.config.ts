bb0a96c826febea320d4800dbf074941b1aa68a2
Frank Riccobono <F.Riccobono@gmail.com>
2026-08-02 13:27:40 -0400

    ELint 10, TS 6 and some vite deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
@@ -9,7 +8,8 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react(), tsconfigPaths(), tanstackRouter()],
+  tsconfigPaths: true,
+  plugins: [react(), tanstackRouter()],
   test: {
     environment: "jsdom",
     globals: true,
